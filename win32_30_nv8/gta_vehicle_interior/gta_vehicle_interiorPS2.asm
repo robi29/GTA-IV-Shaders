aa26b28b23d56ec5dfd0055feff0603271f9fa61
CP <jarema.cp@gmail.com>
2023-04-25 09:23:37 +0200

    [NV8] Slightly increased shadows draw distance 3/n
    hopefully it's done in the right way

--- win32_30_nv8/gta_vehicle_interior/gta_vehicle_interiorPS2.asm
+++ win32_30_nv8/gta_vehicle_interior/gta_vehicle_interiorPS2.asm
@@ -217,7 +217,7 @@
     add r0.y, r0.y, -c53.w
     cmp r0.yw, r0.y, c7.xxzy, c7.xzzw
     mul r0.z, r0.z, r0.z
-    mul r0.z, r0.z, c1.w
+    mul r0.z, r0.z, r0.z
     mad r0.x, r0.x, c3.z, r0.z
     add r0.z, r0.w, r0.x
     cmp_sat r0.x, r0.z, r0.x, r0.y

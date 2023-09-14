166e5662177927672b8a389282850d9bcc237852
robi29 <robinson1994@interia.pl>
2023-09-15 01:07:29 +0200

    Porting z-fighting fix 12/n

--- win32_30_nv8/gta_spec_decal/gta_spec_decalPS11.asm
+++ win32_30_nv8/gta_spec_decal/gta_spec_decalPS11.asm
@@ -100,6 +100,7 @@
 //
 
     ps_3_0
+    def c127, 0.9999999, 1, 0, 0	// LogDepth constants
     def c0, 9.99999975e-006, -0.5, 0.5, -0.25
     def c1, 0.9, 1, 0, 1.5
     def c2, 4, 0.125, 9.99999975e-005, 0
@@ -118,6 +119,7 @@
     dcl_color v3
     dcl_texcoord6 v4.xyz
     dcl vPos.xy
+    dcl_texcoord9 v9
     dcl_2d s0
     dcl_2d s1
     dcl_2d s10
@@ -363,5 +365,19 @@
     add r1.yzw, r1, r3.xxyz
     mad oC0.xyz, r1.x, r1.yzww, r0
     mov oC0.w, r0.w
+	// ----------------------------------------------------------------- Linear2Log -----------------------------------------------------------------
+	if_ne v9.y, c127.y
+		rcp r20.z, c128.x
+		mul r20.x, v9.w, r20.z
+		mul r20.y, c128.y, r20.z
+		log r20.x, r20.x
+		log r20.y, r20.y
+		rcp r20.y, r20.y
+	else
+		mov r20.x, v9.z
+		rcp r20.y, v9.w
+	endif
+	mul oDepth, r20.x, r20.y
+	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 // approximately 247 instruction slots used (16 texture, 231 arithmetic)
--- win32_30_nv8/gta_spec_reflect/gta_spec_reflectPS11.asm
+++ win32_30_nv8/gta_spec_reflect/gta_spec_reflectPS11.asm
@@ -104,6 +104,7 @@
 //
 
     ps_3_0
+    def c127, 0.9999999, 1, 0, 0	// LogDepth constants
     def c0, 9.99999975e-006, 1, -0.5, -0.00999999978
     def c1, 100, -0.5, 0.5, -0.25
     def c2, 0.9, 1, 0, 1.5
@@ -123,6 +124,7 @@
     dcl_color v3
     dcl_texcoord6 v4.xyz
     dcl vPos.xy
+    dcl_texcoord9 v9
     dcl_2d s0
     dcl_2d s1
     dcl_2d s2
@@ -382,5 +384,19 @@
     add r1.yzw, r4.xxyz, r3.xxyz
     mad oC0.xyz, r1.x, r1.yzww, r0
     mov oC0.w, r0.w
+	// ----------------------------------------------------------------- Linear2Log -----------------------------------------------------------------
+	if_ne v9.y, c127.y
+		rcp r20.z, c128.x
+		mul r20.x, v9.w, r20.z
+		mul r20.y, c128.y, r20.z
+		log r20.x, r20.x
+		log r20.y, r20.y
+		rcp r20.y, r20.y
+	else
+		mov r20.x, v9.z
+		rcp r20.y, v9.w
+	endif
+	mul oDepth, r20.x, r20.y
+	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 // approximately 260 instruction slots used (17 texture, 243 arithmetic)

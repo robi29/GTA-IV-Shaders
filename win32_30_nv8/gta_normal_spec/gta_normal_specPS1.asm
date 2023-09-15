ae832c056a1c1265dbad1419e67ad2b2ea681e07
robi29 <robinson1994@interia.pl>
2023-09-15 21:19:01 +0200

    Porting z-fighting fix 22/n

--- win32_30_nv8/gta_normal_spec/gta_normal_specPS1.asm
+++ win32_30_nv8/gta_normal_spec/gta_normal_specPS1.asm
@@ -114,6 +114,7 @@
 //
 
     ps_3_0
+    def c127, 0.9999999, 1, 0, 0	// LogDepth constants
     def c0, -0.5, 9.99999975e-006, 0.5, 0.9
     def c1, 9.99999975e-005, 1.5, 0.25, -0.100000001
     def c2, 0, -1, -0, 1
@@ -134,6 +135,7 @@
     dcl_color v5.xw
     dcl_texcoord6 v6.xyz
     dcl vPos.xy
+    dcl_texcoord9 v9
     dcl_2d s0
     dcl_2d s1
     dcl_2d s2
@@ -399,5 +401,19 @@
     mad r1.xyz, r1.x, r1.yzww, c43
     mad r1.xyz, r2, -r2.w, r1
     mad oC0.xyz, r0.w, r1, r0
+	// ----------------------------------------------------------------- Linear2Log -----------------------------------------------------------------
+	if_ne v9.y, c127.y
+		rcp r20.z, c128.x
+		mul r20.x, v9.w, r20.z
+		mul r20.y, c128.y, r20.z
+		log r20.x, r20.x
+		log r20.y, r20.y
+		rcp r20.y, r20.y
+	else
+		mov r20.x, v9.z
+		rcp r20.y, v9.w
+	endif
+	mul oDepth, r20.x, r20.y
+	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 // approximately 287 instruction slots used (17 texture, 270 arithmetic)

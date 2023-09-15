dcfd160f9a0eefc93c847c444bef148b4a77e55c
robi29 <robinson1994@interia.pl>
2023-09-15 21:23:42 +0200

    Porting z-fighting fix 23/n

--- win32_30_nv8/gta_normal_reflect/gta_normal_reflectPS11.asm
+++ win32_30_nv8/gta_normal_reflect/gta_normal_reflectPS11.asm
@@ -100,6 +100,7 @@
 //
 
     ps_3_0
+    def c127, 0.9999999, 1, 0, 0	// LogDepth constants
     def c0, 1, -0.5, 9.99999975e-006, -0.00999999978
     def c1, 100, -0.5, 0.5, -0.25
     def c2, 0.9, 1, 0, 1.5
@@ -121,6 +122,7 @@
     dcl_color v5
     dcl_texcoord6 v6.xyz
     dcl vPos.xy
+    dcl_texcoord9 v9
     dcl_2d s0
     dcl_2d s1
     dcl_2d s2
@@ -341,5 +343,19 @@
     cmp r1, -r1.y, -c2.y, -c2.z
     texkill r1
     mov oC0, r0
+	// ----------------------------------------------------------------- Linear2Log -----------------------------------------------------------------
+	if_ne v9.y, c127.y
+		rcp r20.z, c128.x
+		mul r20.x, v9.w, r20.z
+		mul r20.y, c128.y, r20.z
+		log r20.x, r20.x
+		log r20.y, r20.y
+		rcp r20.y, r20.y
+	else
+		mov r20.x, v9.z
+		rcp r20.y, v9.w
+	endif
+	mul oDepth, r20.x, r20.y
+	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 // approximately 220 instruction slots used (17 texture, 203 arithmetic)
